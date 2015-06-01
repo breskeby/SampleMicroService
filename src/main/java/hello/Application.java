47aa31f73c0249a380cbb98538213bc2441661cd
Rene Groeschke <rene@breskeby.com>
2015-06-02 00:27:21 +0200

    allow customization of http port via env (needed for heroku)

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,15 +1,21 @@
 package hello;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
+
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
