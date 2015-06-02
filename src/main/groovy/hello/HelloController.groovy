f390cb5d08928a7841744f89538127efafd8809e
Rene Groeschke <rene@breskeby.com>
2015-06-02 12:59:47 +0200

    fix tests

--- src/main/groovy/hello/HelloController.groovy
+++ src/main/groovy/hello/HelloController.groovy
@@ -1,4 +1,5 @@
 package hello
+
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,15 +9,14 @@ class HelloController {
 
     @RequestMapping("/")
     String index() {
-        getApplicationVersion();
-        return "<p style='align=center'><h1>Hello from Gradle Summit!</h1><font size='2'>(version ${getApplicationVersion()})</font></p>";
+        return "<p style='align=center'><h1>Greetings from Gradle Summit!</h1><font size='2'>(version ${getApplicationVersion()})</font></p>";
     }
 
     private String getApplicationVersion() {
         try {
             final URL resource = getClass().getClassLoader().getResource("hello/versioninfo.properties");
             Properties props = new Properties()
-                    props.load(resource.openConnection().inputStream)
+            props.load(resource.openConnection().inputStream)
             return props.getProperty(VERSION_INFO);
         } catch (IOException E) {
             // handle
--- src/test/groovy/hello/HelloControllerTest.java
+++ src/test/groovy/hello/HelloControllerTest.java
@@ -1,9 +1,5 @@
 package hello;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +11,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MockServletContext.class)
@@ -32,6 +32,6 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Gradle Summit!")));
+				.andExpect(content().string(containsString("Greetings from Gradle Summit!")));
 	}
 }
