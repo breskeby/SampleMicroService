3a6763b4c2f7846932592fa1e1403452c301c16b
Rene Groeschke <rene@breskeby.com>
2015-06-03 23:53:23 +0200

    add weather information; format html

--- src/main/groovy/hello/HelloController.groovy
+++ src/main/groovy/hello/HelloController.groovy
@@ -1,7 +1,13 @@
 package hello
 
+import groovy.json.JsonSlurper
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.RestTemplate
 
 @RestController
 class HelloController {
@@ -9,7 +15,80 @@ class HelloController {
 
     @RequestMapping("/")
     String index() {
-        return "<p style='align=center'><h1>Greetings from Gradle Summit!</h1><font size='2'>(version ${getApplicationVersion()})</font></p>";
+        def weather = getWeather()
+        return htmlFormatted("Greetings from Gradle Summit!", weather, getApplicationVersion());
+    }
+
+    String htmlFormatted(String welcomeMessage, def weather, String applicationVersions) {
+        return """
+<html>
+    <head>
+        <style>
+
+            body {
+
+               height: 100%;
+
+               background: #405c72;
+               background-color: #405c72;
+
+               font-family: 'Lato', sans-serif;
+               font-size: 36px;
+               font-weight: normal;
+               text-align: center;
+
+               color: #ffffff; }
+
+            .wrapper {
+                min-height: 100%;
+                height: auto !important;
+                height: 100%;
+                margin: 0 auto -4em;}
+
+            .footer, .push {
+                height: 4em;}
+
+        </style>
+    </head>
+    <body>
+        <div class="wrapper">
+            <p align="center">
+                <h1 color="white">$welcomeMessage</h1>
+                Weather: ${weather.description}<br/>
+            </p>
+            <div class="push"></div>
+        </div>
+        <div class="footer">
+            <p>Copyright (c) 2015, v. <em>$applicationVersions</em></p>
+        </div>
+    </body>
+</html>
+
+"""
+    }
+
+    private def getWeather() {
+        def restTemplate = new RestTemplate()
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Mashape-Key", "Rrh1EvZ43jmsh2xjoYW6XKFN6xsRp18e5GyjsnxMHQm2OGbkWS");
+        headers.set("Other-Header", "othervalue");
+
+        HttpEntity entity = new HttpEntity(headers);
+
+        def url = "https://community-open-weather-map.p.mashape.com/weather?callback=test&lang=en&q=Santa+Clara%2C+CA&units=%22metric%22"
+        try {
+            HttpEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class)
+
+            def body = response.body
+            def json = body.substring(body.indexOf("{"), body.lastIndexOf(")"));
+            def slurper = new JsonSlurper().parseText(json)
+
+            return [description: slurper.weather.description[0]]
+        }catch(HttpServerErrorException exception){
+            return [description: "Weather Service is Down"]
+        }
+
     }
 
     private String getApplicationVersion() {
